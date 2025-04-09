e5043ade37bc88d7f3f9ea3b2b6eb445df2036fa
Gabriel H. P. Leite <145567743+Capita1@users.noreply.github.com>
2025-04-09 12:15:17 -0300

    banco de dados

---
+++ onu.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE onu;
+USE onu;
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL,
+    telefone VARCHAR(20) NOT NULL,
+    data DATE NOT NULL,
+    pais VARCHAR(100) NOT NULL,
+    org VARCHAR(100) NOT NULL,
+    sexo ENUM('M', 'F') NOT NULL
+);
